b2510ca2940c9a3366c48d82c1e0a06ed7449a4d
Aashaya Aryal <aryalaashaya@gmail.com>
2023-12-28 12:44:40 -0600

    introducing header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StyledEngineProvider,
+  createTheme,
+  ThemeProvider,
+} from '@mui/material/styles';
+
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+  },
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1280,
+    },
+  },
+});
 
 export default () => {
   return (
     <React.Fragment>
-      <h1>hi there from aashaya</h1>
-      <hr />
-      <MarketingApp />
+      <ThemeProvider theme={theme}>
+        <StyledEngineProvider injectFirst>
+          <BrowserRouter>
+            <div>
+              <Header />
+              <MarketingApp />
+            </div>
+          </BrowserRouter>
+        </StyledEngineProvider>
+      </ThemeProvider>
     </React.Fragment>
   );
 };
